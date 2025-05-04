4d8455de6c9fe220af8a1728106c8942efac9777
Felipe <felipebs10@gmail.com>
2025-05-04 12:36:07 -0300

    Criado arquivo para tratamento de erros

---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,72 @@
+package resterr
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int      `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (r *RestErr) Error() string {
+	/*Feito essa função para retornar o erro, para evitar conflito com erros de bibliotecas*, nesse formato virá da interface.*/
+	return r.Message
+}
+
+/*Criando um construtor*/
+func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+// Criando os métodos - Para validar as requests
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
