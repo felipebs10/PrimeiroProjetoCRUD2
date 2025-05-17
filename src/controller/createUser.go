5d526fc1d2ce6a49f436af8b6f4e3c26419a912a
Felipe <felipebs10@gmail.com>
2025-05-17 19:09:00 -0300

    Criado os objetos e as validação

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,20 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/felipebs10/PrimeiroProjetoCRUD2/src/configuration/rest_err"
+	"github.com/felipebs10/PrimeiroProjetoCRUD2/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var UserRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorret fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+	}
 }
